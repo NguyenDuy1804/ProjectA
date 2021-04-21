e2209cefaee9664417df894f56d74b7a7e02e146
np119411 <81149391+np119411@users.noreply.github.com>
2021-04-21 11:45:23 +0700

    Add files via upload
    dữ liệu mẫu cho employee và film

---
+++ data.sql
@@ -0,0 +1,14 @@
+﻿-- film
+insert into film values(01, 'A Walk To Remember', 'Adam Shankman', 102, 'Romantic, Drama', 7.4)
+insert into film values(02, 'The Amazing Spider-Man', 'Marc Webb', 136, 'SuperHero, Sci-fi, Romantic', 6.9)
+insert into film values(03, 'All the Bright Places', 'Brett Haley', 107, 'Romantic, Drama', 6.5)
+-- employee
+insert into employee values (01, N'Đặng Tuấn Anh', 0, '0919235789', 'tuananh671@gmail.com', N'19920812' , 'Full Time')
+insert into employee values (02, N'Trần Thị Minh Châu', 1, '0376527172', 'chautran192@gmail.com', N'19920201', 'Full Time')
+insert into employee values (03, N'Nguyễn Lê Hiếu', 0, '0367768923', 'hieupro687@gmail.com', N'19960812', 'Part Time')
+insert into employee values (04, N'Phạm Xuân Hoà', 0, '0366817772', 'xuanhoa223@gmail.com', N'19980223', 'Part Time')
+insert into employee values (05, N'Nguyễn Vũ Gia Hưng', 0, '0943112123', 'hungnguyen84@gmail.com', N'19920804', 'Full Time')
+insert into employee values (06, N'Đỗ Hoàng Gia Bảo', 0, '0373841861', 'baobao678@gmail.com', N'19950524', 'Full Time')
+insert into employee values (07, N'Mạc Thị Ngân Hà', 1, '0362838291', 'hacute212@gmail.com', N'19980212', 'Part Time')
+insert into employee values (08, N'Phạm Tiến Dũng', 0, '0383191252', 'dungpham9821@gmail.com', N'19980201', 'Part Time')
+
